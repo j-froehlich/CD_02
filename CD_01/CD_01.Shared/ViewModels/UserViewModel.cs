8ad9e8601373c5dc35338cbe49f30d22b0624b4f
j-froehlich <j-froehlich@users.noreply.github.com>
2021-06-20 20:40:55 +0200

    model init

---
+++ CD_01/CD_01.Shared/ViewModels/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using CD_01.Models;
+
+namespace CD_01.ViewModels
+{
+    class UserViewModel
+    {
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+        }
+    }
+}
